74e87880a29e17ca0f01f5ab89ddb1a00c77d238
wangchong <wangchong@guoshanggroup.com>
2018-02-01 15:57:15 +0800

    页面

---
+++ src/main/java/com/wangchong/blog/entity/SystemInfo.java
@@ -0,0 +1,16 @@
+package com.wangchong.blog.entity;
+
+import lombok.Data;
+
+@Data
+public class SystemInfo {
+
+    private Long id;
+
+    private Integer opt1;
+
+    private Integer opt2;
+
+    private Integer opt3;
+
+}
