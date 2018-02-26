3e5d591acab9daeb9c49c40afc46c7e9418b0ced
wangchong <wangchong@guoshanggroup.com>
2018-02-26 18:00:57 +0800

    更改富文本编辑器

--- src/main/java/com/wangchong/blog/util/UploadUtil.java
+++ src/main/java/com/wangchong/blog/util/UploadUtil.java
@@ -2,15 +2,25 @@ package com.wangchong.blog.util;
 
 import com.qiniu.util.Auth;
 
+import java.io.File;
+
 public class UploadUtil  {
 
-    private static final String ACCESSKEY = "";
-    private static final String SECRETKEY = "";
-    private static final String BUCKET = "";
+    private static final String ACCESSKEY = "hjgatNKQcJ5pWwOcnj-5KkgnBq3053TSi8f0DIpK";
+    private static final String SECRETKEY = "cJiFBpCdqKqLNOXclI0kHNPajKSn-JHPbcEek32S";
+    private static final String BUCKET = "images";
 
     public static void upload(){
         Auth auth = Auth.create(ACCESSKEY, SECRETKEY);
         String upToken = auth.uploadToken(BUCKET);
+        System.out.println(upToken);
 
+
+
+
+    }
+
+    public static void main(String[] args) {
+        upload();
     }
 }
